7d0437249e0b9514ef4c6117e7036edb318f363d
bialka104b <bialka104b@interia.pl>
2019-08-01 15:23:26 +0200

    coments

--- script/count-numbers.js
+++ script/count-numbers.js
@@ -1,3 +1,11 @@
+// $(document).ready(function(){
+//     $(window).bind('scroll', function () {
+//         var num = 
+//         if ($(window).scrollTop() > num) {
+
+//         }
+//     });
+// });
 $('.number').each(function() {
     $(this).prop('Counter', 0).animate({
       Counter: $(this).text()
